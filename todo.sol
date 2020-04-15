d8e494bbcb469969fa7f02a67491d0610e55133e
Jason Pham <htpham@deakin.edu.au>
2020-04-15 15:08:15 +1000

    Add some setters and getters with modifiers

--- todo.sol
+++ todo.sol
@@ -3,31 +3,60 @@ pragma solidity >=0.5.0 <0.6.0;
 contract ToDo {
     
     struct ToDoItem {
-     string owner;
+     string ownerName;
      string task;
      uint32 dueDate;
+     bool done;
     }
+    
     
     ToDoItem[] public toDoItems;
     
     mapping (uint => address) public itemByOwner;
-    mapping (address => uint) itemCountByOwner;
+    
+    uint private itemCount = 0;
     
     function addToDoItem(string memory _ownerName, string memory _task, uint _amountOfTime) public {
-        uint id = toDoItems.push(ToDoItem(_ownerName, _task, uint32(now + _amountOfTime))) - 1;
+        uint id = toDoItems.push(ToDoItem(_ownerName, _task, uint32(now + _amountOfTime), false)) - 1;
         itemByOwner[id] = msg.sender;
-        itemCountByOwner[msg.sender]++;
+        itemCount++;
     }
     
-    function getItemsByOwner(address _owner) external view returns(uint[] memory) {
-        uint[] memory result = new uint[](itemCountByOwner[_owner]);
-        uint counter = 0;
-        for (uint i = 0; i < toDoItems.length; i++) {
-            if (itemByOwner[i] == _owner) {
-                result[counter] = i;
-                counter++;
-            }
-        }
-        return result;
+    modifier onlyOwner(uint _itemId) {
+        require(msg.sender == itemByOwner[_itemId], 'Invalid node');
+        _;
+    }
+    
+    modifier itemExist(uint _itemId) {
+        require(_itemId < itemCount, 'Item does not exist');
+        _;
+    }
+    
+    function setDone(uint _itemId) public onlyOwner(_itemId) itemExist(_itemId){
+        toDoItems[_itemId].done = true;
+    }
+    
+    function setTask(uint _itemId, string memory _newTask) public onlyOwner(_itemId) itemExist(_itemId) {
+        toDoItems[_itemId].task = _newTask;
+    }
+    
+    function setDueDateExtension(uint _itemId, uint _extensionDays) public onlyOwner(_itemId) itemExist(_itemId) {
+        toDoItems[_itemId].dueDate = toDoItems[_itemId].dueDate + uint32(_extensionDays);
+    }
+    
+    function getItemsOwnerName(uint _itemId) public view itemExist(_itemId) returns(string memory) {
+        return toDoItems[_itemId].ownerName;
+    }
+    
+    function getItemsTask(uint _itemId) public view itemExist(_itemId) returns(string memory) {
+        return toDoItems[_itemId].task;
+    }
+    
+    function getItemsDueDate(uint _itemId) public view itemExist(_itemId) returns(uint) {
+        return toDoItems[_itemId].dueDate;
+    }
+    
+    function getItemsStatus(uint _itemId) public view itemExist(_itemId) returns(bool) {
+        return toDoItems[_itemId].done;
     }
 }
